95ff098a99037d3059397a799d07f8fe06d1c0f6
MarilynLSerna <160423192+MarilynLSerna@users.noreply.github.com>
2024-10-23 12:59:08 -0700

    Date/Time Assignment

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Print current date and time
+        Console.WriteLine("Current date and time: " + DateTime.Now);
+
+        // Ask user for a number
+        Console.WriteLine("Enter a number of hours: ");
+        int hours = Convert.ToInt32(Console.ReadLine());
+
+        // Add the user-entered number to the current time and display the future time
+        DateTime futureTime = DateTime.Now.AddHours(hours);
+        Console.WriteLine("In {0} hours, the time will be: {1}", hours, futureTime);
+
+        Console.ReadLine();
+    }
+}
