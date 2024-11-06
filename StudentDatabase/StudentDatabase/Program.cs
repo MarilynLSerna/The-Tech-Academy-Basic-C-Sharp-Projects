77f8eec2f1c283f59a111fa1ffe66388783d1048
MarilynLSerna <160423192+MarilynLSerna@users.noreply.github.com>
2024-11-06 13:29:10 -0800

    Student Data Base Assignment

---
+++ StudentDatabase/StudentDatabase/Program.cs
@@ -0,0 +1,27 @@
+﻿using StudentDatabase;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        using (var context = new StudentContext())
+        {
+            // Create a new student
+            var student = new Student
+            {
+                FirstName = "Obanai",
+                LastName = "Iguro",
+                EnrollmentDate = DateTime.Now
+            };
+
+            // Add the student to the context
+            context.Students.Add(student);
+
+            // Save the changes to the database
+            context.SaveChanges();
+
+            Console.WriteLine("Student added successfully!");
+        }
+    }
+}
---
+++ StudentDatabase/StudentDatabase/StudentContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabase
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base("StudentDbContext") { }
+        public DbSet<Student> Students { get; set; }
+    }
+}
